06f10dae53842ca97425ad1beb84c8b7ffc100e3
thompsonb <thompsonb@users.noreply.github.com>
2020-12-01 09:18:07 -0500

    moved modified SequenceScorer to prism.py. should fix #3.

--- prism.py
+++ prism.py
@@ -10,12 +10,9 @@ from typing import List, Dict, Iterator, Any, Tuple
 import numpy as np
 import sentencepiece as spm
 import torch
-from fairseq import checkpoint_utils
+from fairseq import checkpoint_utils, utils
 from fairseq.data import LanguagePairDataset
 from sacrebleu import get_source_file, get_reference_files, DATASETS, get_langpairs_for_testset
-
-# modified version of fairseq.sequence_scorer.SequenceScorer which adds softmax temperature control
-from modified_sequence_scorer import SequenceScorer
 
 logger = logging.getLogger('prism')
 logger.setLevel(logging.INFO)
@@ -311,6 +308,147 @@ def main():
             print(ss)
     else:
         print(scores)
+
+
+class SequenceScorer(object):
+    """
+    Copy of https://github.com/pytorch/fairseq/blob/master/fairseq/sequence_scorer.py
+    with softmax temperature control added 
+
+    MIT License
+    
+    Copyright (c) Facebook, Inc. and its affiliates.
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+    
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+    """
+
+    def __init__(self, tgt_dict, softmax_batch=None, temperature=1.0):
+        self.pad = tgt_dict.pad()
+        self.eos = tgt_dict.eos()
+        self.softmax_batch = softmax_batch or sys.maxsize
+        self.temperature = temperature
+        assert self.softmax_batch > 0
+
+    @torch.no_grad()
+    def generate(self, models, sample, **kwargs):
+        """Score a batch of translations."""
+        net_input = sample['net_input']
+
+        def batch_for_softmax(dec_out, target):
+            # assumes decoder_out[0] is the only thing needed (may not be correct for future models!)
+            first, rest = dec_out[0], dec_out[1:]
+            bsz, tsz, dim = first.shape
+            if bsz * tsz < self.softmax_batch:
+                yield dec_out, target, True
+            else:
+                flat = first.contiguous().view(1, -1, dim)
+                flat_tgt = target.contiguous().view(flat.shape[:-1])
+                s = 0
+                while s < flat.size(1):
+                    e = s + self.softmax_batch
+                    yield (flat[:, s:e],) + rest, flat_tgt[:, s:e], False
+                    s = e
+
+        def gather_target_probs(probs, target):
+            probs = probs.gather(
+                dim=2,
+                index=target.unsqueeze(-1),
+            )
+            return probs
+
+        orig_target = sample['target']
+
+        # compute scores for each model in the ensemble
+        avg_probs = None
+        avg_attn = None
+        for model in models:
+            model.eval()
+            decoder_out = model.forward(**net_input)
+            attn = decoder_out[1]
+            if type(attn) is dict:
+                attn = attn.get('attn', None)
+
+            batched = batch_for_softmax(decoder_out, orig_target)
+            probs, idx = None, 0
+            for bd, tgt, is_single in batched:
+                sample['target'] = tgt
+
+                # divide the logits by temperature prior to softmax
+                # for example, see https://github.com/pytorch/fairseq/blob/master/fairseq/sequence_generator.py:
+                #   decoder_out[0][:, -1:, :].div_(temperature)
+                bd[0].div_(self.temperature)
+
+                curr_prob = model.get_normalized_probs(bd, log_probs=len(models) == 1, sample=sample).data
+                if is_single:
+                    probs = gather_target_probs(curr_prob, orig_target)
+                else:
+                    if probs is None:
+                        probs = curr_prob.new(orig_target.numel())
+                    step = curr_prob.size(0) * curr_prob.size(1)
+                    end = step + idx
+                    tgt_probs = gather_target_probs(curr_prob.view(tgt.shape + (curr_prob.size(-1),)), tgt)
+                    probs[idx:end] = tgt_probs.view(-1)
+                    idx = end
+                sample['target'] = orig_target
+
+            probs = probs.view(sample['target'].shape)
+
+            if avg_probs is None:
+                avg_probs = probs
+            else:
+                avg_probs.add_(probs)
+            if attn is not None and torch.is_tensor(attn):
+                attn = attn.data
+                if avg_attn is None:
+                    avg_attn = attn
+                else:
+                    avg_attn.add_(attn)
+        if len(models) > 1:
+            avg_probs.div_(len(models))
+            avg_probs.log_()
+            if avg_attn is not None:
+                avg_attn.div_(len(models))
+
+        bsz = avg_probs.size(0)
+        hypos = []
+        start_idxs = sample['start_indices'] if 'start_indices' in sample else [0] * bsz
+        for i in range(bsz):
+            # remove padding from ref
+            ref = utils.strip_pad(sample['target'][i, start_idxs[i]:], self.pad) \
+                if sample['target'] is not None else None
+            tgt_len = ref.numel()
+            avg_probs_i = avg_probs[i][start_idxs[i]:start_idxs[i] + tgt_len]
+            score_i = avg_probs_i.sum() / tgt_len
+            if avg_attn is not None:
+                avg_attn_i = avg_attn[i]
+                alignment = utils.extract_hard_alignment(avg_attn_i, sample['net_input']['src_tokens'][i],
+                                                         sample['target'][i], self.pad, self.eos)
+            else:
+                avg_attn_i = alignment = None
+            hypos.append([{
+                'tokens': ref,
+                'score': score_i,
+                'attention': avg_attn_i,
+                'alignment': alignment,
+                'positional_scores': avg_probs_i,
+            }])
+        return hypos
 
 
 if __name__ == '__main__':
